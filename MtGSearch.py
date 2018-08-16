8ba1658c693b0ed251886710127d02f55cc54828
Scarv3s <zayed.mzr@gmail.com>
2018-08-16 15:01:32 +0400

    startup

--- MtGSearch.py
+++ MtGSearch.py
@@ -1,13 +1,12 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import selenium
-import urllib.requests
 
-cardName = input("Card Name")
-cardName = cardName.replace(' ', '-').lower()
+cardName = input("Card Name: ")
+cardName = cardName.replace(' ', '+').lower()
 cardName = cardName.replace("'", '%27').lower()
+cardName = cardName.replace(",", '%2C')
 
 print(cardName)
 
 input()
- 
